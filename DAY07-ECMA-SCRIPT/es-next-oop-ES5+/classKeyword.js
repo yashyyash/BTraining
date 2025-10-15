ca1657e7fd96660486d14741ba3cb163e0aa7e52
Yash <yashnil532@gmail.com>
2025-10-15 11:43:38 +0530

    es6 methods

--- DAY07-ECMA-SCRIPT/es-next-oop-ES5+/classKeyword.js
+++ DAY07-ECMA-SCRIPT/es-next-oop-ES5+/classKeyword.js
@@ -23,21 +23,27 @@
 //Internally this is a function [Syntactic Sugar]
 //They do not pollute the global/window scope
 class Person {
-  constructor(){
+  constructor(fname,lname,city,aadharNo){
     console.log("Person Class Constructor Executed!");
+    this.#_socialId = aadharNo;
+    this.firstName = fname;
+    this.city = city;
+    this.lastName = lname;
   }
   #_socialId;
   firstName;
   lastName;
   city;
+  aadharno;
   getPersonInfo() {
-    return `Person ${this.firstName} ${this.lastName} lives in city ${this.city}!`;
+    return `Person ${this.firstName} ${this.lastName} lives in city ${this.city}! social number is ${this.#_socialId}`;
   }
 }
 
-const p1=new Person();
-p1.firstName="Manish";
-p1.lastName="Kaushik";
-p1.city="Raipur";
+const p1=new Person("mainish","kaushik","mum",123);
+// p1.firstName="Manish";
+// p1.lastName="Kaushik";
+// p1.city="Raipur";
+// p1.aadharNo="1234";
 console.log(p1.getPersonInfo());
 console.log(typeof(Person));
