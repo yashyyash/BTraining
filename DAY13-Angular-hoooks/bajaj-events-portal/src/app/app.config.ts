fea3606b2cf75493af4c5996e86764d54545546c
Yash Mutatkar <yashnil532@gmail.com>
2025-10-25 17:55:50 +0530

    first SPA Developed

--- DAY13-Angular-hoooks/bajaj-events-portal/src/app/app.config.ts
+++ DAY13-Angular-hoooks/bajaj-events-portal/src/app/app.config.ts
@@ -1,11 +1,13 @@
 import { provideHttpClient } from '@angular/common/http';
-import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { routes } from './app.routes';
+import { provideRouter } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideHttpClient()
+    provideHttpClient(),
+    provideRouter(routes),
   ]
 };
