87c0afb5555af154bbb00ef316f944b678521558
Yash Mutatkar <yashnil532@gmail.com>
2025-10-25 16:31:37 +0530

    working navbar and footer

--- DAY13-Angular-hoooks/bajaj-events-portal/src/app/app.ts
+++ DAY13-Angular-hoooks/bajaj-events-portal/src/app/app.ts
@@ -1,13 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { EventList } from "./features/events/components/event-list/event-list";
 import { EmployeeList } from "./features/employees/components/employee-list/employee-list";
+import { Navbar } from "./shared/components/navbar/navbar";
+import { Footer } from "./shared/components/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [EventList, EmployeeList],
+  imports: [EventList, EmployeeList, Navbar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
 export class App {
   protected readonly title = signal('bajaj-events-portal');
 }
