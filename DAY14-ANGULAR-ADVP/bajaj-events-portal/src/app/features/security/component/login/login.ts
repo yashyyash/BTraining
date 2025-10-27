d07be09b0aa1f3780db71c9f754abbdfe87d66bf
Yash Mutatkar <yashnil532@gmail.com>
2025-10-27 13:53:46 +0530

    new data

--- DAY14-ANGULAR-ADVP/bajaj-events-portal/src/app/features/security/component/login/login.ts
+++ DAY14-ANGULAR-ADVP/bajaj-events-portal/src/app/features/security/component/login/login.ts
@@ -1,33 +1,53 @@
-import { Component , inject} from '@angular/core';
-
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 import { AuthRequest } from '../../models/auth-request';
 import { AuthResponse } from '../../models/auth-response';
 import { SecurityApi } from '../../services/security-api';
-import { InvokeFunctionExpr } from '@angular/compiler';
 
 @Component({
   selector: 'app-login',
+  standalone: true,
   imports: [FormsModule],
   templateUrl: './login.html',
-  styleUrl: './login.css',
-})
-
+  styleUrls: ['./login.css'],
+  })
+  
 export class Login {
-    private _securityApi = inject(SecurityApi);
-    protected user : AuthRequest = new AuthRequest();
-    protected authResponse : AuthResponse ;
+  private _securityApi = inject(SecurityApi);
+  private _router = inject(Router);
 
+  protected user: AuthRequest = new AuthRequest();
+  protected authResponse?: AuthResponse;
+  protected authErrorMessage: string = '';
+  protected isLoading: boolean = false;
 
-    protected onCredentialSubmit():void{
-      // console.log(this.user);
-      this._securityApi.authenticateCredentials(this.user).subscribe({
-        next:response => {
-          console.log(response);
+  ngOnInit(): void {
+    // Initialization logic if needed
+  }
+
+  protected onCredentialSubmit(): void {
+    this.isLoading = true;
+    this._securityApi.authenticateCredentials(this.user).subscribe({
+      next: (response) => {
+        this.isLoading = false;
+        if (response.token) {
+          this._router.navigate(['/home']);
+        } else {
+          this.authErrorMessage = response.message;
+          setTimeout(() => {
+            this.authErrorMessage = '';
+          }, 5000);
         }
-      });
-    }
-
-
+      },
+      error: (err) => {
+        this.isLoading = false;
+        this.authErrorMessage = 'Authentication failed. Please try again.';
+        setTimeout(() => {
+          this.authErrorMessage = '';
+        }, 5000);
+      }
+    });
+  }
 }
