dec77af308f03313e1248278b09c9bb867de7486
Yash Mutatkar <yashnil532@gmail.com>
2025-10-27 12:27:56 +0530

    new data

--- DAY14-ANGULAR-ADVP/bajaj-events-portal/src/app/app.routes.ts
+++ DAY14-ANGULAR-ADVP/bajaj-events-portal/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { EpHome } from "./features/home/ep-home/ep-home";
 import { EmployeeList } from "./features/employees/components/employee-list/employee-list";
 import { EventList } from "./features/events/components/event-list/event-list";
 import { ResorceNotFound } from "./shared/components/resorce-not-found/resorce-not-found";
+import { Login } from "./features/security/component/login/login";
 export const routes :Routes= [
     {
         path:" ",//default Route
@@ -28,6 +29,11 @@ export const routes :Routes= [
         path:"events/:id",
         loadComponent:()=>import("./features/events/components/event-details/event-details").then(ed=>ed.EventDetails),
         title:"Event Details"
+    },
+    {
+        path:"login",
+        component : Login,
+        title :"Login"
     },
     {
         path:"**",
